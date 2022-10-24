d379d93249399c03072a29bb9d5cc8cca5bd7603
Wellington <wellington.batista@lightbase.com.br>
2022-10-24 08:51:12 -0300

    inicializando projeto

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,13 @@
+import './Banner.css'
+
+function Banner() {
+    //JSX
+    return (
+        <header className='banner'>
+            <img src="/image/banner.png" alt="O banner principal"></img>
+        </header>
+    )
+
+}
+
+export default Banner
